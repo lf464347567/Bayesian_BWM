606259d68132051c2f6fba0a99e24927330041b5
lf464347567 <59129647+lf464347567@users.noreply.github.com>
2024-10-09 14:26:14 +0800

    Delete tl_compute.m

--- tl_compute.m
+++
@@ -1,7 +0,0 @@
-function y = tl_compute(vk,x_va,ib)
-    if ib>= Dis(vk,x_va) || (sum(sum(x_va == vk)) == size(vk,1)*size(vk,2))
-        y = 1;
-    else
-        y = ib/Dis(vk,x_va);
-    end
-end
